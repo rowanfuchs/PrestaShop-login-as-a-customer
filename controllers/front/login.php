1f3a600bc2cf67e62f685ab40d010bc0fd5bd8c6
Rowan Fuchs <info@cloudcanyon.nl>
2020-07-25 00:04:18 +0200

    removed customer context and added registerSession

--- controllers/front/login.php
+++ controllers/front/login.php
@@ -44,15 +44,14 @@ class LoginAsCustomerLoginModuleFrontController extends ModuleFrontControllerCor
             $customer = new Customer((int) $id_customer);
             if (Validate::isLoadedObject($customer)) {
                 $customer->logged = 1;
-                $this->context->customer = $customer;
                 $this->context->cookie->id_customer = (int) $customer->id;
                 $this->context->cookie->customer_lastname = $customer->lastname;
                 $this->context->cookie->customer_firstname = $customer->firstname;
-                $this->context->cookie->logged = 1;
-                $this->context->cookie->check_cgv = 1;
-                $this->context->cookie->is_guest = $customer->isGuest();
                 $this->context->cookie->passwd = $customer->passwd;
+                $this->context->cookie->logged = 1;
                 $this->context->cookie->email = $customer->email;
+                $this->context->cookie->is_guest = $customer->isGuest();
+                $this->context->cookie->registerSession(new CustomerSession());
                 Tools::redirect('index.php?controller=my-account');
             }
         }
